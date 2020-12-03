fc9661c2735e8efde216bf6154ac64051a19d0ff
Julien Béguin <julien.beguin99@gmail.com>
2020-12-03 16:34:08 +0100

    add file's headers

--- app/src/main/java/ch/heigvd/iict/sym/labo3/NfcActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo3/NfcActivity.kt
@@ -1,3 +1,10 @@
+/**
+ * SYM : Labo 3 - Environnement I. Codes-barres, iBeacons et NFC
+ * Auteurs : Julien Béguin, Robin Cuénoud & Gaëtan Daubresse
+ * Date : 03.12.2020
+ * Classe : B
+ */
+
 package ch.heigvd.iict.sym.labo3
 
 import androidx.appcompat.app.AppCompatActivity
