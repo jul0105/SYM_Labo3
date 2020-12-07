db888180f1d8c84a03740560751c44a42e589398
Gaetan <gaetan.daubresse@gmail.com>
2020-12-07 13:29:59 +0100

    BarCode Activity completed

--- app/src/main/java/ch/heigvd/iict/sym/labo3/BarCodeActivity.kt
+++ app/src/main/java/ch/heigvd/iict/sym/labo3/BarCodeActivity.kt
@@ -7,12 +7,61 @@
 
 package ch.heigvd.iict.sym.labo3
 
-import androidx.appcompat.app.AppCompatActivity
+import android.Manifest
+import android.content.pm.PackageManager
+import android.graphics.Color
 import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.ResultPoint
+import com.journeyapps.barcodescanner.BarcodeCallback
+import com.journeyapps.barcodescanner.BarcodeResult
+import com.journeyapps.barcodescanner.DecoratedBarcodeView
+import com.journeyapps.barcodescanner.DefaultDecoderFactory
+import java.util.*
+
 
 class BarCodeActivity : AppCompatActivity() {
+    private lateinit var barcodeView: DecoratedBarcodeView
+    private var scannedText = ""
+
+    private val callback: BarcodeCallback = object : BarcodeCallback {
+        override fun barcodeResult(result: BarcodeResult) {
+            // Check if already scanned
+            if (result.text == scannedText) {
+                return
+            }
+            scannedText = result.text
+            barcodeView.setStatusText(result.text)
+
+            // Add image of scanned barcode
+            val imageView =
+                findViewById<ImageView>(R.id.scannedBarcode)
+            imageView.setImageBitmap(result.getBitmapWithResultPoints(Color.YELLOW))
+        }
+
+        override fun possibleResultPoints(resultPoints: List<ResultPoint>) {}
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_bar_code)
+
+        // Check permissions
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 1)
+        }
+
+        barcodeView = findViewById(R.id.barcode_scanner)
+        val formats: Collection<BarcodeFormat> =
+        Arrays.asList(BarcodeFormat.QR_CODE, BarcodeFormat.CODE_39)
+        barcodeView.barcodeView.decoderFactory = DefaultDecoderFactory(formats)
+        barcodeView.initializeFromIntent(intent)
+        barcodeView.decodeContinuous(callback)
+        barcodeView.resume()
     }
+
 }
